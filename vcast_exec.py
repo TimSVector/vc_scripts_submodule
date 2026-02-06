bcda83bd0ef048bd476b18a70e243296ca011d50
TimSVector <tim.schneider@vector.com>
2026-02-06 14:15:01 -0500

    Updates for reporting

--- vcast_exec.py
+++ vcast_exec.py
@@ -396,18 +396,29 @@ class VectorCASTExecute(object):
 
     def reportCreate(self, report_type, desc):
         from vector.apps.DataAPI.vcproject_api import VCProjectApi
+        from vector.apps.DataAPI.cover_api import CoverApi
         vcproj = VCProjectApi(self.FullMP)
         
+        forCover = {"FULL_REPORT": "AGGREGATE_REPORT",
+                    "MANAGEMENT_REPORT": "COVER_MANAGEMENT_REPORT"}
         for env in vcproj.Environment.all():
             if not env.is_active:
                 continue
 
             self.needIndexHtml = True
 
-            report_name = env.compiler.name + "_" + env.testsuite.name + "_" + env.name + "_management_report.html"
-            report_name = os.path.join(self.output_dir, "management",report_name)
-            print("Creating {} HTML report for {} in {}".format(desc, env.name, report_name))
-            env.api.report(report_type=report_type, formats=["HTML"], output_file=report_name)
+            if "MANAGEMENT_REPORT" in report_type:
+                report_name = env.compiler.name + "_" + env.testsuite.name + "_" + env.name + "_management_report.html"
+                report_name = os.path.join(self.output_dir, "management",report_name)
+                print("Creating {} HTML report for {} in {}".format(desc, env.name, report_name))
+            else:
+                report_name = env.compiler.name + "_" + env.testsuite.name + "_" + env.name + "_full_report.html"
+                report_name = os.path.join(self.output_dir, "management",report_name)
+                print("Creating {} HTML report for {} in {}".format(desc, env.name, report_name))
+            if isinstance(env.api, CoverApi):
+                env.api.report(report_type=forCover[report_type], formats=["HTML"], output_file=report_name)
+            else:
+                env.api.report(report_type=report_type, formats=["HTML"], output_file=report_name)
 
         vcproj.close()
         
