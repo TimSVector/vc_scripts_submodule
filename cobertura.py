3c3ae3f5760945f20b6a875dd278d7aa025aa0ed
TimSVector <tim.schneider@vector.com>
2026-06-16 13:05:49 -0400

    Updates for pathing

--- cobertura.py
+++ cobertura.py
@@ -108,23 +108,9 @@ def getFileXML(testXml, coverAPI, verbose = False, extended = False, source_root
         except:
             prj_dir = os.getcwd().replace("\\","/") + "/"
 
+
     fname = coverAPI.display_name
-    fpath = coverAPI.display_path
-    print(f"[INFO-1.0] fpath = {fpath}")
-    isRelPath =  ".." in fpath
-    print(f"[INFO-2.0] is fpath have .. in it = {fpath}")
-    if ".." in fpath:
-        print(f"[INFO-2.1] fixing fpath = {fpath}")
-        fpath = coverAPI._relative_path    
-        print(f"[INFO-2.2] to fpath = {fpath}")
-        
-    print(f"[INFO-3.0]fpath = {fpath}")
-    try:
-        fpath = os.path.relpath(fpath,prj_dir).replace("\\","/")
-    except:
-        fpath = fpath.replace("\\","/")
-        pass
-    print(f"[INFO-3.0]fpath = {fpath}")        
+    fpath = coverAPI._relative_path.replace("\\","/")
 
     branch_totals = float(coverAPI.metrics.branches + coverAPI.metrics.mcdc_branches)
     branch_covered = float(
